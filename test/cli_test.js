b33f0c19863e9eb8731713ae869560feb8a537a5
david.bashford <dbashford@bericotechnologies.com>
2015-08-10 22:41:48 -0400

    add test for cli

---
+++ test/cli_test.js
@@ -0,0 +1,17 @@
+var exec = require("child_process").exec
+  , path = require("path")
+  , cliPath = path.join(__dirname, "..", "bin", "textract")
+  , testFilePath = path.join(__dirname, "files", "css.css")
+  ;
+
+describe("cli", function(){
+  it("will extract text", function(done) {
+    exec( cliPath + " " + testFilePath,
+      function( error, stdout, stderr ) {
+        expect(stdout.indexOf(".foo {color:red}")).to.be.gt(90);
+        done();
+      }
+    );
+  })
+});
+
